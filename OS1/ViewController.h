04b87074b0c05f36d985b9bbd0a29c6e00668c5d
Admin <admin@Mac-Admin.local>
2014-10-10 12:02:07 +0400

    Initial Commit

---
+++ OS1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OS1
+//
+//  Created by Admin on 10.10.14.
+//  Copyright (c) 2014 Soult. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
